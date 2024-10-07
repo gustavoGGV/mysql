2710988b226b3366983ea2f7b9b5df2c0d920646
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-10-07 15:25:40 -0300

    Update basico.sql

--- basico.sql
+++ basico.sql
@@ -1,12 +1,50 @@
-create database aula;
+create database universidade;
 show databases;
-use aula;
-create table medico
+use universidade;
+
+create table professor
 (
-	codMed int PRIMARY KEY,
-    nomeMed varchar(40) NOT NULL,
-    dataNasc date NOT NULL,
-    cpf char(11),
-    cidade varchar(30) NOT NULL,
-    especialidade varchar(30) NOT NULL
+	codProf int PRIMARY KEY,
+    nomeProf varchar(40) NOT NULL,
+    areaProf varchar(40) NOT NULL,
+    cpfProf char(11) NOT NULL,
+    dataNascProf date NOT NULL
+);
+
+use universidade;
+
+create table disciplina
+(
+	codDisc int PRIMARY KEY,
+    nomeDisc varchar(40) NOT NULL,
+    cargaHorDisc int NOT NULL
+);
+
+use universidade;
+
+create table ministra
+(
+	codProf int NOT NULL,
+    codDisc int NOT NULL,
+    PRIMARY KEY (codProf, codDisc),
+    FOREIGN KEY(codProf) references professor(codProf),
+    FOREIGN KEY(codDisc) references disciplina(codDisc)
+);
+
+create database teste;
+use teste;
+
+/* seguir esta formatação: */
+
+create table cliente(
+	codCli int primary key,
+    nomeCli varchar(40) not null,
+    enderecoCli varchar(40) not null,
+    dataNacCli date
+);
+
+create table produto(
+	codProd int primary key,
+    nomeProd varchar(40),
+    valorProd float
 );
